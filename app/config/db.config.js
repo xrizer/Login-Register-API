b0cc4cd316c31d1bfe7aad85ec05cf700df9ef07
xrizer <aqsholafifi@gmail.com>
2022-06-11 08:45:36 +0700

    auth api

---
+++ app/config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    HOST: "localhost",
+    USER: "root",
+    PASSWORD: "mike429A",
+    DB: "anjayy",
+    dialect: "mysql",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  };
